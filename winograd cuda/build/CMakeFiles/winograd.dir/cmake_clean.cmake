6ae78f79a6436de31c3c7300b24d91ae46c39b24
KuaiLeBozi <47180764+KuaiLeBozi@users.noreply.github.com>
2025-04-03 09:09:51 +0800

    Add files via upload

---
+++ winograd cuda/build/CMakeFiles/winograd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/winograd.dir/driver.cu.o"
+  "CMakeFiles/winograd.dir/driver.cu.o.d"
+  "winograd"
+  "winograd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/winograd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
